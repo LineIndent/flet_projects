9b57e20c7a6fcbedfa14984ea9c35a807616de02
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-06-12 12:02:19 +0200

    Adding Flet projects to repo. Status: OK.

--- ft_advanced_navbar_104/main.py
+++ ft_advanced_navbar_104/main.py
@@ -1,7 +1,52 @@
 import flet as ft
 
 
+class Header(ft.UserControl):
+    def __init__(self):
+        self.header = ft.Container(
+            height=90,
+            bgcolor="teal",
+            padding=ft.padding.only(left=60, right=60),
+            shadow=ft.BoxShadow(
+                spread_radius=2,
+                blur_radius=4,
+                color=ft.colors.with_opacity(0.25, "black"),
+                offset=ft.Offset(2, 2),
+            ),
+            animate=ft.Animation(500, "ease"),
+            content=ft.Column(
+                controls=[
+                    # Title section ...
+                    ft.Row(controls=[ft.Text("Name Or Title", size=21)]),
+                    # Navigation section ...
+                    ft.Row(
+                        controls=[
+                            ft.Text("Home", size=9),
+                            ft.Text("About", size=9),
+                            ft.Text("Contact", size=9),
+                        ]
+                    ),
+                ]
+            ),
+        )
+        super().__init__()
+
+    def build(self):
+        return self.header
+
+
 def main(page: ft.Page):
+    #
+    header = Header()
+
+    page.add(
+        ft.Stack(
+            controls=[
+                header,
+            ]
+        )
+    )
+
     page.update()
 
 
