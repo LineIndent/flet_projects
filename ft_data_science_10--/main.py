965f3d3526b0dc2117ce2ca8854969dd1188e79a
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-06-16 16:50:16 +0200

    In progress...

--- ft_data_science_10--/main.py
+++ ft_data_science_10--/main.py
@@ -2,6 +2,7 @@ import flet as ft
 import pandas as pd
 from math import pi
 
+
 # Step 2: Importing and Loading the Data
 sales_data = pd.read_csv("ft_data_science_10--/sales.csv")
 
@@ -43,32 +44,14 @@ for MONTH in sales_data.columns[2:]:
 #         pass
 
 
-def main(page: ft.Page):
-    chart = DataVisualization(103123.33, 370000.00, 60)
-    chart1 = DataVisualization(263123.33, 370000.00, 80)
-    chart2 = DataVisualization(123123.33, 370000.00, 100)
-    page.add(
-        ft.Stack(
-            [
-                chart,
-                chart1,
-                chart2,
-            ]
-        )
-    )
-    page.update()
-
-
 class DataVisualization(ft.UserControl):
-    def __init__(self, month_sum: float, max_sum: float, spacing: int):
+    def __init__(self, month_sum: float, max_sum: float, spacing: int, color: str):
         self.month_sum: float = month_sum
         self.delta_sum: float = max_sum - self.month_sum
 
         self.chart: ft.Control = ft.PieChart(
             sections=[
-                # dynamic PieSection circle
-                ft.PieChartSection(value=self.month_sum, color="teal600", radius=15),
-                # background PieSection circle
+                ft.PieChartSection(value=self.month_sum, color=color, radius=15),
                 ft.PieChartSection(
                     value=self.delta_sum,
                     color=ft.colors.with_opacity(0.025, "white"),
@@ -77,14 +60,68 @@ class DataVisualization(ft.UserControl):
             ],
             sections_space=0,
             center_space_radius=spacing,
-            expand=True,
             rotate=ft.Rotate((pi) / 2),
-            # on_chart_event=lambda e: self.hover_circle(e),
         )
+
         super().__init__()
+
+    def get_event(self, e):
+        print(self.month_sum)
 
     def build(self):
         return self.chart
+
+
+def main(page: ft.Page):
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+
+    colors = []
+    for num in range(1, 13):
+        colors.append(f"teal{num}00")
+
+    # Step 2: Importing and Loading the Data
+    sales_data = pd.read_csv("ft_data_science_10--/sales.csv")
+
+    # process and clean the data
+    sales_data = sales_data.replace("n.a.", pd.NA)
+    sales_data = sales_data.dropna()
+
+    # if columns are strings, convert them to the right dtypes for summation
+    for MONTH in sales_data.columns[5:]:
+        sales_data[MONTH] = sales_data[MONTH].str.replace(",", "")
+        sales_data[MONTH] = sales_data[MONTH].str.replace("not available", "0")
+        sales_data[MONTH] = sales_data[MONTH].str.replace("not avilable", "0")
+        sales_data[MONTH] = pd.DataFrame(sales_data, columns=[MONTH], dtype=float)
+
+    max_num = []
+    for MONTH in sales_data.columns[5:]:
+        max_num.append(sales_data[MONTH].sum())
+    max_num = max(max_num)
+
+    stack = ft.Stack(scale=ft.Scale(0.60))
+    size = 60
+    for index, MONTH in enumerate(sales_data.columns[5:]):
+        stack.controls.append(
+            DataVisualization(
+                month_sum=sales_data[MONTH].sum(),
+                max_sum=max_num,
+                spacing=size,
+                color=colors[index],
+            )
+        )
+        size += 20
+
+    page.add(
+        ft.Container(
+            width=500,
+            height=500,
+            bgcolor=ft.colors.with_opacity(0.005, "white10"),
+            content=stack,
+        )
+    )
+
+    page.update()
 
 
 if __name__ == "__main__":
